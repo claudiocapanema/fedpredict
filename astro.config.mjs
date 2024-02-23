fde9748e151283ad9588377aa774c6c8beab7556
claudiocapanema <60147132+claudiocapanema@users.noreply.github.com>
2024-02-23 11:59:03 -0300

    Delete astro.config.mjs

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config'
-
-export default defineConfig({
-  site: 'https://claudiocapanema.github.io',
-  base: '/fedpredict',
-    outDir: 'public',
-  publicDir: 'static',
-})
